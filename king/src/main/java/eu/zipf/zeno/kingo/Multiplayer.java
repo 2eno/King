5a55415a0126615bddd78543f091e68f64c438dd
2eno <zeno.zipf@gmail.com>
2018-03-16 03:41:01 +0100

    ..

--- king/src/main/java/eu/zipf/zeno/kingo/Multiplayer.java
+++ king/src/main/java/eu/zipf/zeno/kingo/Multiplayer.java
@@ -1,10 +1,112 @@
 package eu.zipf.zeno.kingo;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.games.multiplayer.realtime.OnRealTimeMessageReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import eu.zipf.zeno.kingo.bc.R;
+
 /**
  * Created by zeno on 11.03.18.
  */
 
 public class Multiplayer {
+        /*
+     * COMMUNICATIONS SECTION. Methods that implement the game's network
+     * protocol.
+     */
 
+
+    private static Activity ACTIVITY = new Activity();
+
+    static void setActivity(Activity activity) {
+        ACTIVITY = activity;
+    }
+
+    // Score of other participants. We update this as we receive their scores
+    // from the network.
+    static Map<String, Integer> mParticipantScore = new HashMap<>();
+
+    // Participants who sent us their final score.
+    static Set<String> mFinishedParticipants = new HashSet<>();
+
+
+    public static OnRealTimeMessageReceivedListener mOnRealTimeMessageReceivedListener = new OnRealTimeMessageReceivedListener() {
+        @Override
+        public void onRealTimeMessageReceived(@NonNull RealTimeMessage realTimeMessage) {
+            byte[] buf = realTimeMessage.getMessageData();
+            String sender = realTimeMessage.getSenderParticipantId();
+            String log = "Message recieved: ";
+            for (byte b : buf) {
+                log += ", " + b;
+            }
+            Log.d(MainActivity.TAG, log);
+
+
+            switch (buf[0]) {
+                case 10:                                                                            //10 is GAME_RESULT
+                    UpdateUI.updateDice(buf);
+                    break;
+                case 11:
+                    MainActivity.myTurn();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+
+    static void broadcast(ArrayList<Byte> msg, String mRoomId) {
+        PlayAPI.mRealTimeMultiplayerClient.sendUnreliableMessageToOthers(castByte(msg.toArray()), mRoomId);
+    }
+
+    static void broadcast(byte[] msg, String mRoomId) {
+        PlayAPI.mRealTimeMultiplayerClient.sendUnreliableMessageToOthers(msg, mRoomId);
+    }
+
+    static void broadcast(Byte msg, String mRoomId) {
+        ArrayList<Byte> l = new ArrayList<Byte>();
+        l.add(msg);
+        broadcast(l, mRoomId);
+    }
+
+    static void broadcastReset(String mRoomId) {
+        byte[] toSend = {10, 1, 1, 1, 1, 1, 1};
+        broadcast(toSend, mRoomId);
+    }
+
+    static byte[] castByte(Object[] o) {
+        byte[] ret = new byte[o.length];
+        int i = 0;
+        for (Object obj : o) {
+            ret[i] = (byte) obj;
+            i++;
+        }
+        return ret;
+    }
+
+    static byte[] getReroll() {
+        ArrayList toCast = new ArrayList();
+        for (int i = 0; i < MainActivity.getDice().getSize(); i++) {
+            ToggleButton b = ACTIVITY.findViewById(R.id.dice_1 + i);
+            if (!b.isChecked()) {
+                toCast.add((byte) i);
+            }
+        }
+        byte[] ret = castByte(toCast.toArray());
+
+        return ret;
+    }
 }
 
--- king/src/main/java/eu/zipf/zeno/kingo/UpdateUI.java
+++ king/src/main/java/eu/zipf/zeno/kingo/UpdateUI.java
@@ -2,6 +2,7 @@ package eu.zipf.zeno.kingo;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import eu.zipf.zeno.kingo.bc.R;
@@ -10,11 +11,12 @@ import eu.zipf.zeno.kingo.bc.R;
  * Created by 2eno on 11.03.18.
  */
 
-public class UpdateUI {
+public final class UpdateUI {
 
-    static Activity activity = new Activity();
-    static void setActivity(Activity _activity){
-        activity = _activity;
+    static Activity ACTIVITY = new Activity();
+
+    static void setActivity(Activity activity) {
+        ACTIVITY = activity;
     }
 
     /*
@@ -37,19 +39,19 @@ public class UpdateUI {
     static int mCurScreen = -1;
 
     static int[] getClickables() {
-           return CLICKABLES;
+        return CLICKABLES;
     }
 
-    static void switchToScreen(int screenId) {
+    static void switchToScreen(int screenId, PlayAPI playAPI) {
         // make the requested screen visible; hide all others.
         for (int id : SCREENS) {
-            activity.findViewById(id).setVisibility(screenId == id ? View.VISIBLE : View.GONE);
+            ACTIVITY.findViewById(id).setVisibility(screenId == id ? View.VISIBLE : View.GONE);
         }
         mCurScreen = screenId;
 
         // should we show the invitation popup?
         boolean showInvPopup;
-        if (MainActivity.mIncomingInvitationId == null) {
+        if (playAPI.mIncomingInvitationId == null) {
             // no invitation, so no popup
             showInvPopup = false;
 
@@ -57,27 +59,64 @@ public class UpdateUI {
             // single-player: show on main screen and gameplay screen
             showInvPopup = (mCurScreen == R.id.screen_main);
         }
-        activity.findViewById(R.id.invitation_popup).setVisibility(showInvPopup ? View.VISIBLE : View.GONE);
+        ACTIVITY.findViewById(R.id.invitation_popup).setVisibility(showInvPopup ? View.VISIBLE : View.GONE);
     }
 
-    static void switchToMainScreen() {
-        if (MainActivity.mRealTimeMultiplayerClient != null) {
-            switchToScreen(R.id.screen_main);
+    static void switchToMainScreen(PlayAPI playAPI) {
+        if (playAPI.mRealTimeMultiplayerClient != null) {
+            switchToScreen(R.id.screen_main, playAPI);
         } else {
-            switchToScreen(R.id.screen_sign_in);
+            switchToScreen(R.id.screen_sign_in, playAPI);
         }
     }
 
     // updates the label that shows my score
     static void updateScoreDisplay() {
-        ((TextView) activity.findViewById(R.id.my_score)).setText("Test");
+        ((TextView) ACTIVITY.findViewById(R.id.my_score)).setText("Test");
     }
-
 
 
     // updates the screen with the scores from our peers
     static void updatePeerScoresDisplay() {
 
+    }
+    static void forMyTurn() {
+        ACTIVITY.findViewById(R.id.button_next).setVisibility(View.GONE);
+        ACTIVITY.findViewById(R.id.button_roll).setEnabled(true);
+        ACTIVITY.findViewById(R.id.button_roll).setVisibility(View.VISIBLE);
+
+    }
+
+    static void updateDice(byte[] scores) {
+        byte modifier = 0;
+        if (scores[0] == 10) {
+            modifier = 1;
+        }
+        for (int i = modifier; i < (scores.length); i++) {
+            Button b = ACTIVITY.findViewById(R.id.dice_1 + i - modifier);
+            switch (scores[i]) {
+                case Die.ENERGY:
+                    b.setBackgroundResource(R.drawable.energy);
+                    break;
+                case Die.SMASH:
+                    b.setBackgroundResource(R.drawable.attack);
+                    break;
+                case Die.HEAL:
+                    b.setBackgroundResource(R.drawable.health);
+                    break;
+                case Die.ONE:
+                    b.setBackgroundResource(R.drawable.one);
+                    break;
+                case Die.TWO:
+                    b.setBackgroundResource(R.drawable.two);
+                    break;
+                case Die.THREE:
+                    b.setBackgroundResource(R.drawable.three);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
 }
